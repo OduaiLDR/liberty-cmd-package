84ee2e5f4306ad5a06d76011f66cab3eef2ee730
Oduai Abrb <oduai@libertydebtrelief.com>
2025-11-06 02:51:20 +0300

    Changed To Connector code to laravel command structure

--- src/ReportsServiceProvider.php
+++ src/ReportsServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Cmd\Reports;
 
 use Illuminate\Support\ServiceProvider;
+use Cmd\Reports\Console\Commands\TestSnowflakeJWT;
 
 class ReportsServiceProvider extends ServiceProvider
 {
@@ -10,6 +11,13 @@ class ReportsServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'reports');
+        
+        // Register console commands
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                TestSnowflakeJWT::class,
+            ]);
+        }
     }
 
     public function register()
